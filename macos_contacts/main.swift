c157e1bf7d1acfd57639bfee57eb116330c2f07d
Shawn Morris <shawn@smorris.com>
2017-02-07 09:52:50 -0600

    Program now seems to function.

--- macos_contacts/main.swift
+++ macos_contacts/main.swift
@@ -7,6 +7,20 @@
 //
 
 import Foundation
+import Contacts
 
-print("Hello, World!")
+if CommandLine.argc < 2 {
+    fputs("usage: macos_contacts <search string>)\n", stderr)
+} else {
+    let store = CNContactStore()
+    let contacts = try store.unifiedContacts(matching: CNContact.predicateForContacts(matchingName: CommandLine.arguments[1]), keysToFetch:[CNContactGivenNameKey as CNKeyDescriptor, CNContactFamilyNameKey as CNKeyDescriptor, CNContactEmailAddressesKey as CNKeyDescriptor])
 
+    for contact in contacts {
+        // Checking if phone number is available for the given contact.
+        if (contact.isKeyAvailable(CNContactEmailAddressesKey)) {
+            for emailAddress:CNLabeledValue in contact.emailAddresses {
+                print("\(emailAddress.value)    \(contact.givenName) \(contact.familyName)")
+            }
+        }
+    }
+}
