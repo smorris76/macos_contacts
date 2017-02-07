6e2d6f6d02735ff5c93482fc78096a47f453df95
Shawn Morris <shawn@smorris.com>
2017-02-07 09:00:24 -0600

    Initial Commit

---
+++ macos_contacts/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  macos_contacts
+//
+//  Created by Shawn Morris on 2/7/17.
+//  Copyright © 2017 Shawn Morris. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
